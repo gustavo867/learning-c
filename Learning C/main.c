8114127b5f2585a22ec8934de4f2c3c5d314bbbb
Gustavo Santana <gustavo@madatech.com.br>
2021-10-07 17:15:27 -0300

    Initial Commit

---
+++ Learning C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Learning C
+//
+//  Created by Mac on 07/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
